d0a73fe9f5a159f3620c41ba513e9cd1ecdaf465
Tomas <medinatomas@sanluis.edu.ar>
2023-09-26 10:35:54 -0300

    primer commit

---
+++ src/Clase2.java
@@ -0,0 +1,10 @@
+public class Clase2 {
+    public static void main(String[] args) {
+        String saludar = "Hola como estas ";
+        String nombre = "Nicolás";
+        System.out.println(saludar + nombre);
+
+        System.out.println("saludar.toLowerCase() = " + saludar.toUpperCase());
+    }
+
+}
---
+++ src/Clase_12_9_23/Ejercicio7.java
@@ -0,0 +1,10 @@
+package Clase_12_9_23;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        /*
+        Pide una cadena porr consola y muestra sus caracteres uno por uno
+  */
+        public static voidd mai
+    }
+}
